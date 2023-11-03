aea2011b085c0c146090723925a5ebab11f7b6df
Nick Ciraulo <nickopicz@gmail.com>
2023-11-02 22:26:10 -0400

    changed to just get raw data

--- check_ch.py
+++ check_ch.py
@@ -9,23 +9,23 @@ from database_functions import create_table_for_pdo, store_to_db, get_next_trial
 
 value_range_map = {
     # COB-ID, Bytes : (Data Type, Description, Value Range, Units)
-    # (902, (0, 1)): ("U16", "Status word", "0-65535", ""),
+    (902, (0, 1)): ("U16", "Status word", "0-65535", ""),
     (902, (2, 3)): ("S16", "Actual speed", "-32768 to 32767", "Rpm"),
-    # (902, (4, 5)): ("U16", "RMS motor Current", "0-65535", "Arms"),
-    # (902, (6, 7)): ("S16", "DC Bus Voltage", "-32768 to 32767", "Adc"),
-    # (1158, (0, 1)): ("S16", "Internal Speed Reference", "-32768 to 32767", "Rpm"),
-    # (1158, (2, 3)): ("S16", "Reference Torque", "-32768 to 32767", "Nm"),
-    # (1158, (4, 5)): ("S16", "Actual Torque", "-32768 to 32767", "Nm"),
-    # (1158, (6, 7)): ("S16", "Field weakening control: voltage angle", "-32768 to 32767", "Deg"),
-    # (390, 0): ("U8", "Field weakening control: regulator status", "0-255", ""),
-    # (390, 1): ("U8", "Current limit: actual limit type", "0-15", ""),
-    # (390, (2, 3)): ("S16", "Motor voltage control: U peak normalized", "-32768 to 32767", ""),
-    # (390, (4, 5)): ("U16", "Digital status word", "0-65535", ""),
-    # (390, (6, 7)): ("S16", "Scaled throttle percent", "-32768 to 32767", ""),
-    # (646, (0, 1)): ("S16", "Motor voltage control: idLimit", "-32768 to 32767", ""),
-    # (646, (2, 3)): ("S16", "Motor voltage control: Idfiltered", "-32768 to 32767", "Arms"),
-    # (646, (4, 5)): ("S16", "Actual currents: iq", "-32768 to 32767", "Apk"),
-    # (646, (6, 7)): ("S16", "Motor measurements: DC bus current", "-32768 to 32767", "Adc"),
+    (902, (4, 5)): ("U16", "RMS motor Current", "0-65535", "Arms"),
+    (902, (6, 7)): ("S16", "DC Bus Voltage", "-32768 to 32767", "Adc"),
+    (1158, (0, 1)): ("S16", "Internal Speed Reference", "-32768 to 32767", "Rpm"),
+    (1158, (2, 3)): ("S16", "Reference Torque", "-32768 to 32767", "Nm"),
+    (1158, (4, 5)): ("S16", "Actual Torque", "-32768 to 32767", "Nm"),
+    (1158, (6, 7)): ("S16", "Field weakening control: voltage angle", "-32768 to 32767", "Deg"),
+    (390, 0): ("U8", "Field weakening control: regulator status", "0-255", ""),
+    (390, 1): ("U8", "Current limit: actual limit type", "0-15", ""),
+    (390, (2, 3)): ("S16", "Motor voltage control: U peak normalized", "-32768 to 32767", ""),
+    (390, (4, 5)): ("U16", "Digital status word", "0-65535", ""),
+    (390, (6, 7)): ("S16", "Scaled throttle percent", "-32768 to 32767", ""),
+    (646, (0, 1)): ("S16", "Motor voltage control: idLimit", "-32768 to 32767", ""),
+    (646, (2, 3)): ("S16", "Motor voltage control: Idfiltered", "-32768 to 32767", "Arms"),
+    (646, (4, 5)): ("S16", "Actual currents: iq", "-32768 to 32767", "Apk"),
+    (646, (6, 7)): ("S16", "Motor measurements: DC bus current", "-32768 to 32767", "Adc"),
 }
 pdo_map = {
     902: "PDO1",
@@ -119,7 +119,7 @@ def read_can_messages(trial_number):
                 msg = ch.read()
                 pdo_label = pdo_map.get(msg.id, "Unknown_PDO")
                 msg_data = format_can_message(msg)
-
+                print("raw data: ", msg)
                 in_memory_data.append({
                     'pdo_label': pdo_label,
                     'trial_number': trial_number,
@@ -129,19 +129,19 @@ def read_can_messages(trial_number):
                 # create_table_for_pdo(pdo_label)
                 # store_to_db(pdo_label, trial_number, msg)
                 # print(f"\n{pdo_label} (COB-ID: {msg.id:X}):")
-                for desc, (value, value_range, units) in msg_data['data_values'].items():
-                    os.system("cls")
-                    if (desc == "Scaled throttle percent"):
-                        print("Actual throttle percent: ", value/32767)
-                    if (desc == "Actual speed"):
-                        print("Real speed scaled: ", value/1000)
-                    if (desc == "DC Bus Voltage"):
-                        print("Actual Voltage: ", value*0.01)
-                    if (desc == "Actual Torque"):
-                        print("Actual Torque: ", value*0.1, " nm")
-                    if (desc == "Internal Speed Reference"):
-                        print("Speed Reference: ", value)
-                    # print(f"{desc}: {value} {units} (Range: {value_range})")
+                # for desc, (value, value_range, units) in msg_data['data_values'].items():
+                # os.system("cls")
+                # if (desc == "Scaled throttle percent"):
+                #     print("Actual throttle percent: ", value/32767)
+                # if (desc == "Actual speed"):
+                #     print("Real speed scaled: ", value/1000)
+                # if (desc == "DC Bus Voltage"):
+                #     print("Actual Voltage: ", value*0.01)
+                # if (desc == "Actual Torque"):
+                #     print("Actual Torque: ", value*0.1, " nm")
+                # if (desc == "Internal Speed Reference"):
+                #     print("Speed Reference: ", value)
+                # print(f"{desc}: {value} {units} (Range: {value_range})")
             except canlib.CanNoMsg:
 
                 pass  # No new message yet
