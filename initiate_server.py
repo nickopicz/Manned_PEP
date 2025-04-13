23f2e0770d310d47058a0e9504ae8848aadc5d46
Jack Ceriello <jackceriello@Jacks-MacBook-Pro.local>
2025-04-12 21:12:36 -0400

    unknown

--- initiate_server.py
+++ initiate_server.py
@@ -79,7 +79,7 @@ def get_data():
 if __name__ == '__main__':
     # Setup ngrok
     ngrok_tunnel = ngrok.connect(
-        addr=5000, hostname="bold-privately-koala.ngrok-free.app")
+        addr=5000, hostname="sitboatworks.ngrok.dev")
     print('NGROK Tunnel URL:', ngrok_tunnel.public_url)
 
     # Run Flask app
